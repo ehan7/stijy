6ba3b9b79e82f5cff64cff60c36cab925788fe63
ehan7 <han.ethan.07@gmail.com>
2022-04-06 01:48:05 -0500

    Initial Commit

---
+++ stijy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  stijy
+//
+//  Created by Ethan Han on 4/6/22.
+//  Copyright © 2022 Group4. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
